01273369ebeeff2661ab2e55d477c333b93eaf0c
Vitor Campos <vitor.natal@timwe.com>
2023-12-28 11:57:22 -0300

    first commit

---
+++ FaceFact/FaceFact/FaceFactApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactApp.swift
+//  FaceFact
+//
+//  Created by Vitor Campos on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
